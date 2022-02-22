de8ceb023892dc3ca7def3aefd1e53ccc62d4154
Oinari-Shinto <oinari.shinto@gmail.com>
2022-02-23 00:41:47 +0300

    add custom Fonts and async function

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [ fontLoaded, setFontLoaded ] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading 
+    startAsync={fetchFonts} 
+    onFinish={() => setFontLoaded(true)}
+    />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>New Menu App!</Text>
