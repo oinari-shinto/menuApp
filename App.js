a3c43122b265ac232284d907d4b502137c2a13a7
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-04 23:55:18 +0300

    fix AppLoding component , install AppLoading

--- App.js
+++ App.js
@@ -2,29 +2,34 @@ import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import  {AppLoading}  from 'expo';
 
 import MealsNavigator from './navigation/MealsNavigator';
 
-const fetchFonts = () => {
+/* const fetchFonts = () => {
   return Font.loadAsync ({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
+}; */
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
 };
-
 export default function App() {
-  const [ dataLoaded, setDataLoaded ] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
   if (!dataLoaded) {
     return (
-      <AppLoading 
-        startAsync={fetchFonts} 
-        onFinish={() => setDataLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
+    <AppLoading 
+    startAsync = {fetchFonts} 
+    onFinish={() => setDataLoaded(true)}
+    onError={(err) => console.log(err)}
+    />
     );
-  }
+  };
 
   return  <MealsNavigator />;
   
