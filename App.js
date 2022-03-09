2c9c0c360a51d846703125bd75a324d313ddc2a3
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-09 23:51:23 +0300

    add @react-navigation/stack

--- App.js
+++ App.js
@@ -12,16 +12,16 @@ import MealsNavigator from './navigation/MealsNavigator';
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 }; */
-/* const fetchFonts = () => {
+const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
   });
-}; */
+};
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
 
-  /* if (!dataLoaded) {
+  if (!dataLoaded) {
     return (
     <AppLoading 
     startAsync = {fetchFonts} 
@@ -29,10 +29,10 @@ export default function App() {
     
     />
     );
-  }; */
+  };
   /* <MealsNavigator /> */
   return <View>
-    <Text>WEwe</Text>
+    <MealsNavigator />
 
   </View>
   
