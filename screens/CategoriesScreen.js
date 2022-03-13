93421a1d3bea0d936932557f8e583a1202202654
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-13 22:40:00 +0300

    add TouchableOpacity

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,28 +1,44 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES } from '../data/dummy-data'
 
-const renderGridItem = (itemData) => {
-  return <View style={styles.gridItem}><Text>{itemData.item.title}</Text></View>;
-};
-
-const CategoriesScreen = (props) => {
+const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'CategoryMeals' })
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
   return (
-    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    gridItem: {
-      flex: 1,
-      margin: 15,
-    }
-}); 
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15
+  }
+})
 
 export default CategoriesScreen
