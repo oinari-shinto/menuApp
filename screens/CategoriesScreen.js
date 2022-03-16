bbc0dadcfbcfbe148f759ce758b839591121a5d4
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-16 23:54:38 +0300

    add params in categotyScreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -17,7 +17,9 @@ const CategoriesScreen = props => {
       <TouchableOpacity
         style={styles.gridItem}
         onPress={() => {
-          props.navigation.navigate({ routeName: 'CategoryMeals' })
+          props.navigation.navigate({ routeName: 'CategoryMeals' params: {
+            categoryId: itemData.item.id
+          } })
         }}
       >
         <View>
