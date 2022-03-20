5c3f5aac40b2366cb86d2a90c5129ae1a896ad33
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-20 20:45:23 +0300

    create default navigation option

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -6,9 +6,23 @@ import CategoryMeals from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 
 const MealsNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
-    CategoryMeals: CategoryMeals,
+    Categories: {
+        screen: CategoriesScreen,
+        navigationOptions: {
+            headerTitle: 'Meal Categories',
+        }
+    } ,
+    CategoryMeals: {
+        screen: CategoryMeals,
+    },
     MealDetail: MealDetailScreen
+},{
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
+    }
 });
 
 export default createAppContainer(MealsNavigator);
