74913e1d49f7f8e2c059bf1dcbaff67de9c8ea6b
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-26 23:17:23 +0300

    add some new list meals

--- components/MealItem.js
+++ components/MealItem.js
@@ -6,10 +6,14 @@ const MealItem = props => {
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={props.onSelect}>
         <View>
-          <View style={styles.mealRow}>
-            <Text>{itemData.item.title}</Text>
+          <View style={{...styles.mealRow, ...styles.mealHeader}}>
+            <Text>{props.title}</Text>
           </View>
-          <View style={styles.mealRow}></View>
+          <View style={{...styles.mealRow, ...styles.mealDetail}}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity}</Text>
+            <Text>{props.affordability}</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
@@ -23,7 +27,16 @@ const styles = StyleSheet.create({
       height: 200,
       width: '100%',
       backgroundColor: '#ccc'
+  },
+  mealHeader: {
+    height: '90%',
+
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between'
   }
+
 })
 
 export default MealItem
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,47 +1,57 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList, Platform } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Platform } from 'react-native'
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
-import Colors from '../constants/Colors';
-import MealItem from '../components/MealItem';
+import { CATEGORIES, MEALS } from '../data/dummy-data'
+import Colors from '../constants/Colors'
+import MealItem from '../components/MealItem'
 
-const CategoryMealsScreen = (props) => {
+const CategoryMealsScreen = props => {
   const renderMealsItem = itemData => {
-    return(
-      <MealItem />
-    );
-  };
-  const catId = props.navigation.getParam('categoryId');
+    return <MealItem 
+    title={itemData.item.title}
+    duration={itemData.item.duration} 
+    complexity={itemData.item.complexity}
+    affordability={itemData.item.affordability}
+    onSelect={() => {}} />
+  }
+  const catId = props.navigation.getParam('categoryId')
 
-  const displayedMeals = MEALS.find(meal => meal.categoryIds.indexOf(catId) >= 0 );
+  const displayedMeals = MEALS.find(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  )
   /* const selectedCategory = CATEGORIES.find(cat => cat.id === catId); */
   return (
     <View style={styles.screen}>
-        <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealsItem}/>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealsItem}
+        style={{width: '100%'}}
+      />
     </View>
-  );
-};
+  )
+}
 
-CategoryMealsScreen.navigationOptions = (navigationData) => {
-  const catId = navigationData.navigation.getParam('categoryId');
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId')
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
 
   return {
     headerTitle: selectedCategory.title,
     headerStyle: {
       backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-      },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
-  };
-};
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
+  }
+}
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-}); 
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default CategoryMealsScreen
