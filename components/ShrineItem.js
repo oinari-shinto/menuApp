a90560e242ffc6a243e6b986134c7067d2b68125
Oinari-Shinto <oinari.shinto@gmail.com>
2022-03-31 23:36:20 +0300

    add background image and text

--- components/ShrineItem.js
+++ components/ShrineItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native'
 
 const ShrineItem = props => {
   return (
@@ -7,12 +7,15 @@ const ShrineItem = props => {
       <TouchableOpacity onPress={props.onSelect}>
         <View>
           <View style={{...styles.shrineRow, ...styles.shrineHeader}}>
-            <Text>{props.title}</Text>
+            <ImageBackground source={{url: props.image}} style={bgImage}>
+              <Text style={styles.title} numberOfLines={1}>{props.title}</Text>
+            </ImageBackground>
+            
           </View>
           <View style={{...styles.shrineRow, ...styles.shrineDetail}}>
             <Text>{props.duration}m</Text>
-            <Text>{props.complexity}</Text>
-            <Text>{props.affordability}</Text>
+            <Text>{props.complexity.toUpperCase()}</Text>
+            <Text>{props.affordability.toUpperCase()}</Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -28,6 +31,11 @@ const styles = StyleSheet.create({
       width: '100%',
       backgroundColor: '#ccc'
   },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end'
+  },
   shrineHeader: {
     height: '90%',
 
@@ -35,6 +43,19 @@ const styles = StyleSheet.create({
   shrineDetail: {
     paddingHorizontal: 10,
     justifyContent: 'space-between'
+  },
+  shrineDetail: {
+    paddingHorizontal: 10,
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    textAlign: 'center'
   }
 
 })
