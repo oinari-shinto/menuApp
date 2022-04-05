1bf3e467bb69cf8e8475b91b1b7ee6eeafb9d95a
Oinari-Shinto <oinari.shinto@gmail.com>
2022-04-05 23:02:06 +0300

    add react bottom tabs

--- navigation/ShrinesNavigator.js
+++ navigation/ShrinesNavigator.js
@@ -1,28 +1,38 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryShrines from '../screens/CategoryShrinesScreen';
 import ShrineDetailScreen from '../screens/ShrineDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
-const ShrinesNavigator = createStackNavigator({
+const ShrinesNavigator = createStackNavigator(
+  {
     Categories: {
-        screen: CategoriesScreen,
-        navigationOptions: {
-            headerTitle: 'Shrine Categories',
-        }
-    } ,
+      screen: CategoriesScreen,
+      navigationOptions: {
+        headerTitle: 'Shrine Categories'
+      }
+    },
     CategoryShrines: {
-        screen: CategoryShrines,
+      screen: CategoryShrines
     },
     ShrineDetail: ShrineDetailScreen
-},{
+  },
+  {
     defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Color.primaryColor
     }
+  }
+)
+
+const ShrinesFavTabNavigator = createBottomTabNavigator({
+    Shrines: ShrinesNavigator,
+    Favorites: FavoritesScreen
 });
 
-export default createAppContainer(ShrinesNavigator);
+export default createAppContainer(ShrinesFavTabNavigator)
