0307d35731c9081708d0cb3a9a95695b356389c9
Oinari-Shinto <oinari.shinto@gmail.com>
2022-04-29 22:29:23 +0300

    start create List logic

---
+++ components/ShrineList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+const ShrineList = props => {
+  return (
+    <div>ShrineList</div>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ShrineList;
