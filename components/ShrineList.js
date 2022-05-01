c7242309ac99e931ab9ead324a41e9fdf50c426e
Oinari-Shinto <oinari.shinto@gmail.com>
2022-05-01 22:36:26 +0300

    start create List logic v3

--- components/ShrineList.js
+++ components/ShrineList.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
+import ShrineItem from './ShrineItem';
+
 
 const ShrineList = props => {
+  const renderShrinesItem = itemData => {
+    return <ShrineItem 
+    title={itemData.item.title}
+    image={itemData.item.imageUrl}
+    duration={itemData.item.duration} 
+    complexity={itemData.item.complexity}
+    affordability={itemData.item.affordability}
+    onSelect={() => {
+      props.navigation.navigate({routeName: 'ShrineDetail', params: {
+        shrineId: itemData.item.id
+      }})
+    }} />
+  }
   return (
     <View style={styles.list}>
       <FlatList
